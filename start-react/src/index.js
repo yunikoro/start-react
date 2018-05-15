1270c7d873cc0633f4a80044dd18be4a68814f8e
黄知恒 <huangzhiheng@xiaojubianli.com>
2018-05-15 22:09:11 +0800

    history 没搞出来

--- start-react/src/index.js
+++ start-react/src/index.js
@@ -5,18 +5,30 @@ import './index.css';
 class Game extends React.Component {
     constructor(props) {
        super(props)
+       this.state = {
+           history: [{
+               squares: Array(9).fill(null),
+           }],
+           xIsNext: true,
+       }
+    }
+
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{ /* status */ }</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        )
     }
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            history: [],
-            squares: Array(9).fill(null),
-            xIsNext: true
-        };
-    }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
